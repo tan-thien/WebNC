b4003dac9a9c24002055f570284838813238b162
tan-thien <thiennguyen14062004@gmail.com>
2025-04-13 21:13:43 +0700

    Add project files.

---
+++ WebShopSolution.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebShopSolution.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
